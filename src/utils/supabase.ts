a0eb282c81f7bd57a2819bf5c3078f0bc34710e4
Oelmaier, Mark <oelmaier.mark@gmail.com>
2025-07-22 21:48:37 +0200

    add creds

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,24 +1,26 @@
-import { parseCookies, setCookie } from '@tanstack/react-start/server'
-import { createServerClient } from '@supabase/ssr'
+import { parseCookies, setCookie } from "@tanstack/react-start/server";
+import { createServerClient } from "@supabase/ssr";
 
 export function getSupabaseServerClient() {
-  return createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return Object.entries(parseCookies()).map(([name, value]) => ({
-            name,
-            value,
-          }))
-        },
-        setAll(cookies) {
-          cookies.forEach((cookie) => {
-            setCookie(cookie.name, cookie.value)
-          })
-        },
-      },
-    },
-  )
+    return createServerClient(
+        "https://vodtzsjfnrhpbehyojfb.supabase.co",
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvZHR6c2pmbnJocGJlaHlvamZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEwMTg3NTAsImV4cCI6MjA2NjU5NDc1MH0.awAKuoyn6qJ-ZMps1le438eRKKK6A7aarWnkKhV5p-4",
+        {
+            cookies: {
+                getAll() {
+                    return Object.entries(parseCookies()).map(
+                        ([name, value]) => ({
+                            name,
+                            value,
+                        })
+                    );
+                },
+                setAll(cookies) {
+                    cookies.forEach((cookie) => {
+                        setCookie(cookie.name, cookie.value);
+                    });
+                },
+            },
+        }
+    );
 }
